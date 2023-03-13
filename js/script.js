c735941db10ffdcd7220059b1b24b5a76e55f25b
ijaz35 <ijaz.hossain1@gmail.com>
2023-03-14 00:19:56 +0600

    bug fixed

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
-
+const errorMsg = document.getElementById('err-msg');
 //------------- handle search button-----------
 const searchBook = () => {
-  const searchField = document.getElementById("search-field");
+  const searchField = document.getElementById("search-input");
   const searchText = searchField.value;
+  if (!searchText) {
+    errorMsg.innerHTML = 'Enter book name';
+    return;
+  }
 
+  errorMsg.innerHTML = '';
   // ----------load data----------
   const url = `https://openlibrary.org/search.json?q=${searchText}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displaySearchResult(data.docs));
+    .then((data) => {
+      console.log(data);
+      displaySearchResult(data)
+    });
 };
 
 // ----------display search result data----------
 const displaySearchResult = (myBooks) => {
+  // console.log(myBooks);
   const searchResult = document.getElementById("search-result");
+  const bookCount = document.getElementById("book-num");
   searchResult.innerText = "";
 
+  bookCount.innerHTML = `<h2 class="text-center my-4 text-warning"
+  >Total books found ${myBooks.numFound}</h2>`;
 
-  const books = myBooks.docs;
-  books.forEach((book) => {
+
+
+  myBooks.docs.slice(0, 15).forEach((book) => {
     console.log(book);
-    const div = document.innerHTML("div");
+    const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
         <div class="card">
@@ -28,7 +41,7 @@ const displaySearchResult = (myBooks) => {
             <div class="card-body text-center">
               <h5 class="card-title"> Name: ${book.title}</h5>
               <h6> Author: ${book.author_name}</h6>
-              <p>Publisher: <small> ${book.publisher[0]} </small></p>
+              <p>Publisher: <small> ${book?.publisher ? book?.publisher[0] : 'Not found'} </small></p>
               <small> First Published Year: ${book.first_publish_year}</small>
             </div>
         </div>
